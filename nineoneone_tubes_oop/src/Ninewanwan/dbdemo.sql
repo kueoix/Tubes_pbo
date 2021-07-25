4b96351d9a91dda38d3f8bb7ec144e3a5ef4677e
rendr <53697461+kueoix@users.noreply.github.com>
2021-07-25 21:07:55 +0700

    Add files via upload

---
+++ nineoneone_tubes_oop/src/Ninewanwan/dbdemo.sql
@@ -0,0 +1,11 @@
+USE booking;
+
+CREATE TABLE tabel(
+Nik VARCHAR(10),
+nama VARCHAR(64),
+notelp bigint(25),
+notelp2 bigint(25),
+email VARCHAR(20),
+alamat VARCHAR(100),
+jumlah_rombongan int(3)
+);
